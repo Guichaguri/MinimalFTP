dfc3caa7632ed1eb554dda455a7d4631c07c48fd
Guichaguri <guichaguri@gmail.com>
2017-05-23 13:03:11 -0300

    Added a simple example

---
+++ src/test/java/guichaguri/minimalftp/SimpleServer.java
@@ -0,0 +1,31 @@
+package guichaguri.minimalftp;
+
+import guichaguri.minimalftp.impl.NativeFileSystem;
+import guichaguri.minimalftp.impl.NoOpAuthenticator;
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * A simple API example
+ * @author Guilherme Chaguri
+ */
+public class SimpleServer {
+
+    public static void main(String[] args) throws IOException {
+        // Uses the current working directory as the root
+        File root = new File(System.getProperty("user.dir"));
+
+        // Creates a native file system
+        NativeFileSystem fs = new NativeFileSystem(root);
+
+        // Creates a noop authenticator
+        NoOpAuthenticator auth = new NoOpAuthenticator(fs);
+
+        // Creates the server with the authenticator
+        FTPServer server = new FTPServer(auth);
+
+        // Start listening synchronously
+        server.listenSync(21);
+    }
+
+}
